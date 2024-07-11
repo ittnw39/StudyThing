a0d485b128fe7e31e8e6551eff0f5e1898f02282
우 연정 <yj753110@naver.com>
2024-07-11 22:01:09 +0900

    setting

---
+++ demo/src/main/java/Study_Match/HomeController/HomeController.java
@@ -0,0 +1,13 @@
+package Study_Match.HomeController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "index.html";
+    }
+}
