b4a45a32967015e78e5b422f55a0f5d8d3364c32
우 연정 <yj753110@naver.com>
2024-07-11 21:03:30 +0900

    Normalize line endings

---
+++ demo/src/test/java/Study_Match/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Study_Match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
