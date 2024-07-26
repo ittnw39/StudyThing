f5990ecffae5cca8801d01d1eff3c5e58c648c3c
우 연정 <yj753110@naver.com>
2024-07-27 00:18:31 +0900

    UPDATE login&register

--- demo/src/main/java/Study_Match/HomeController/HomeController.java
+++ demo/src/main/java/Study_Match/HomeController/HomeController.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "index.html";
+    @GetMapping("/")
+    public String showLoginPage() {
+        return "login/index";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register/index";
+    }
+
+    @GetMapping("/findmyacc")
+    public String showFindMyAccountPage() {
+        // 비밀번호 찾기 페이지 경로를 추가
+        return "findmyacc/index";
     }
 }
